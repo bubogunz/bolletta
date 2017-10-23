f455729a44af71f4eee60b713dedf34d4f3f80ed
Elisabetta <30654677+elisabetta-p@users.noreply.github.com>
2017-10-23 17:00:39 +0200

    Update telefonata.cpp

--- telefonata.cpp
+++ telefonata.cpp
@@ -2,17 +2,17 @@
 
 telefonata::telefonata(orario i, orario o, unsigned short int *n): inizio(i), fine(o) {
   int j = 0;
-  numero = new short int[14];
+  numero = new unsigned short int[14];
   while(j<14) { numero[j] = n[j]; j++; }
 }
 telefonata::telefonata(const telefonata& t): inizio(t.inizio), fine(t.fine) {
-  numero = new short int[14];
+  numero = new unsigned short int[14];
   for(int i=0; i<14; i++) numero[i] = t.numero[i];
 }
 orario telefonata::Inizio() const { return inizio; }
 orario telefonata::Fine() const { return fine;  }
-short int *telefonata::Numero() const {  return numero;  }
-bool telefonata::sameNumber(const short int *b, const short int *c) const{
+unsigned short int *telefonata::Numero() const {  return numero;  }
+bool telefonata::sameNumber(const unsigned short int *b, const unsigned short int *c) const{
   int i = 0;
   bool eq = true;
   while(i<14 && eq){
@@ -29,7 +29,7 @@ bool telefonata::operator==(const telefonata& t) const{
 std::ostream& operator<<(std::ostream& os, const telefonata& t){
   os <<  "INIZIO:" << t.Inizio() << " FINE:" <<  t.Fine()
         << " NUMERO CHIAMATO:";
-  short int* f = t.Numero();
+  unsigned short int* f = t.Numero();
   for(int i=0; i<14; i++) os<<f[i];
   return os;
 }
